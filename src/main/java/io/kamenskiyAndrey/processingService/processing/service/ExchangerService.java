0c8023aeb245bcf76d511f326ee66f2593270b5d
AndreyJavaEd <scream123456@yandex.ru>
2023-12-06 06:28:14 +0300

    Начал реализацию метода exchangeCurrency() перевода валюты с одного счета на другой, прописали условия перевода в зависимости в какой валюте находится денежная сумма. Переводы будем проводить в рублях. Далее необходимо реализовать код в условиях.

--- src/main/java/io/kamenskiyAndrey/processingService/processing/service/ExchangerService.java
+++ src/main/java/io/kamenskiyAndrey/processingService/processing/service/ExchangerService.java
@@ -10,16 +10,40 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 public class ExchangerService {
 
+    private static final String CURRENCY_RUB = "RUB";
     private final AccountCreateService service;
 
+    /*
+    Метод перевода валюты с одно счета на другой, в зависимости от условий:
+    если у счета переводчика сумма не в рублях, а у счета получателя в рублях,
+    если у счета переводчика и у счета получателя суммы не в рублях.
+     */
+    public BigDecimal exchangeCurrency(String uuid, Long fromAccount, Long toAccount, BigDecimal ammount){
+        AccountEntity source = service.getAccountById(fromAccount); //получаем объект счета котрый будет отправлять сумму на другой счет
+        AccountEntity target = service.getAccountById(toAccount); // объект счета который будет получать деньги с другого счета
 
+        BigDecimal result;
+        //Проверка если код валют на обоих счетах в РУБЛЯХ.
+        if (CURRENCY_RUB.equals(source.getCurrencyCode()) && CURRENCY_RUB.equals(target.getCurrencyCode())){
+            result = moneyTransferFromOneAccToOther(uuid, source, target, ammount);
+        } else if (!CURRENCY_RUB.equals(source.getCurrencyCode()) && CURRENCY_RUB.equals(target.getCurrencyCode())) {
+            
+        } else if (CURRENCY_RUB.equals(source.getCurrencyCode()) && !CURRENCY_RUB.equals(target.getCurrencyCode())) {
+
+        } else if (!CURRENCY_RUB.equals(source.getCurrencyCode()) && !CURRENCY_RUB.equals(target.getCurrencyCode())) {
+
+        }
+
+
+        return null;
+    }
 
 
 
 
 
     /*
-    Метод перевода валюты с одного счета на другой счет
+    Метод перевода валюты с одного счета на другой счет если валюта с кодом RUB
      */
     public BigDecimal moneyTransferFromOneAccToOther(String uuid, AccountEntity source
             , AccountEntity target, BigDecimal amount) {
